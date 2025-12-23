9531c66ee641076b89c86ae2660521b29e8ef5cc
MaauzMansoor <muhammadmaauz2018@gmail.com>
2025-12-23 13:53:56 +0500

    renderfix

--- server/routes/productRecommendationEngineRoutes.js
+++ server/routes/productRecommendationEngineRoutes.js
@@ -1,50 +1,58 @@
 import express from 'express';
 import {
-  getRecommendations,
-  getById,
-  createRecommendation,
-  updateRecommendation,
-  deleteRecommendation,
-  trainModel,
+  getUserRecommendations,
+  createRecommendationEngine,
   updateUserProfile,
-  getSimilarProducts,
-  getPopularProducts,
-  getCrossSellItems,
-  getUpsellItems,
-  trackInteraction
+  recordImpression,
+  recordClick,
+  recordConversion,
+  getCrossSellRecommendations,
+  getUpSellRecommendations,
+  getTrendingRecommendations,
+  getPersonalizedBundles,
+  getSimilarItems,
+  getRecommendationPerformance,
+  getTopPerformers
 } from '../controllers/productRecommendationEngineController.js';
 import { isAuthenticatedUser, authorizeRoles } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.route('/recommendations')
-  .get(isAuthenticatedUser, getRecommendations)
-  .post(isAuthenticatedUser, createRecommendation);
+  .get(isAuthenticatedUser, getUserRecommendations)
+  .post(isAuthenticatedUser, authorizeRoles('admin'), createRecommendationEngine);
 
-router.route('/recommendations/:id')
-  .get(isAuthenticatedUser, getById)
-  .put(isAuthenticatedUser, updateRecommendation)
-  .delete(isAuthenticatedUser, deleteRecommendation);
-
-router.route('/recommendations/:id/train')
-  .post(isAuthenticatedUser, authorizeRoles('admin'), trainModel);
-
-router.route('/recommendations/:id/profile')
+router.route('/recommendations/profile')
   .post(isAuthenticatedUser, updateUserProfile);
 
 router.route('/recommendations/similar/:productId')
-  .get(getSimilarProducts);
+  .get(getSimilarItems);
 
-router.route('/recommendations/popular')
-  .get(getPopularProducts);
+router.route('/recommendations/trending')
+  .get(getTrendingRecommendations);
+
+router.route('/recommendations/bundles')
+  .get(isAuthenticatedUser, getPersonalizedBundles);
 
 router.route('/recommendations/cross-sell/:productId')
-  .get(getCrossSellItems);
+  .get(getCrossSellRecommendations);
 
 router.route('/recommendations/upsell/:productId')
-  .get(getUpsellItems);
+  .get(getUpSellRecommendations);
 
-router.route('/recommendations/track')
-  .post(trackInteraction);
+router.route('/recommendations/impression')
+  .post(recordImpression);
+
+router.route('/recommendations/click')
+  .post(recordClick);
+
+router.route('/recommendations/conversion')
+  .post(recordConversion);
+
+router.route('/recommendations/performance')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), getRecommendationPerformance);
+
+router.route('/recommendations/top-performers')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), getTopPerformers);
 
 export default router;
